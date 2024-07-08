5e5a02b6397d3c403bf2df721a7692622b901ce5
Пользователь <urinboytursunboev@gmail.com>
2024-07-08 19:23:26 +0500

    update Default index.php

--- modules/dashboard/views/default/index.php
+++ modules/dashboard/views/default/index.php
@@ -1,12 +1,30 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = Yii::t('app', 'Dashboard');
+//$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
 <div class="dashboard-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+
+    <div class="card">
+        <div class="card-body">
+            Dashboardga hush kelibsiz!
+        </div>
+    </div>
+
+<!--    <h1>--><?php //= $this->context->action->uniqueId ?><!--</h1>-->
+<!--    <p>-->
+<!--        This is the view content for action "--><?php //= $this->context->action->id ?><!--".-->
+<!--        The action belongs to the controller "--><?php //= get_class($this->context) ?><!--"-->
+<!--        in the "--><?php //= $this->context->module->id ?><!--" module.-->
+<!--    </p>-->
+<!--    <p>-->
+<!--        You may customize this page by editing the following file:<br>-->
+<!--        <code>--><?php //= __FILE__ ?><!--</code>-->
+<!--    </p>-->
 </div>
